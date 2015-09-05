ff9474894f9a92bdc944c50305596a31f51b80a6
Fahim Chowdhury <fahim.chowdhury@thomsonreuters.com>
2015-09-05 19:10:05 +0100

    added me

--- bot.js
+++ bot.js
@@ -8,16 +8,31 @@ module.exports = function (req, res, next) {
 
     botPayload = {
       userName:'pluto',
-      text : 'Good Morning, @' + channelName + '! Please type "!start" when you are ready to stand up'
+      text : 'Good Morning, @' + channelName + '!\n\r Please type "!start" when you are ready to stand up'
     };
 
-  }
-  if(text == "!start")
+  }else if(text == "!start")
   {
 
     botPayload = {
       userName:'pluto',
-      text : 'Okay lets start!\n\r 1.What did you do yesterday?'
+      text : 'Okay lets start!\n\r 1.What did you do yesterday?\n\r 2.What are you working on today?\n\r 3.Any Blockers?\n\r When you\'re done type "!next!"\n\r\n\r Right so who\'s going first?\n\r To speak next type "!me"'
+    };
+
+  }else if(text == "!next")
+  {
+
+    botPayload = {
+      userName:'pluto',
+      text : 'Okay guys whos next?'
+    };
+
+  }else if(text == "!me")
+  {
+
+    botPayload = {
+      userName:'pluto',
+      text : 'Great, so '+userName+' you\'re next.\n\rYou may start'
     };
 
   }
